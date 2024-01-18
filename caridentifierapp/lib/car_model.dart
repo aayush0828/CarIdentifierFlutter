1dc8948738a3bf403e9335e9b612e763f341840d
Aayush Thakur <132382177+aayush0828@users.noreply.github.com>
2024-01-18 20:00:59 +0530

    Update car_model.dart

--- caridentifierapp/lib/car_model.dart
+++ caridentifierapp/lib/car_model.dart
@@ -10,7 +10,7 @@ class CarModel {
   double maxCityMpg;
   // Add other relevant fields
 
-  CarModel({
+  CarModel({     
     required this.make,
     required this.model,   
     required this.fuelType,
