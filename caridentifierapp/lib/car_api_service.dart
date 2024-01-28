f8f81445e769a6457af63765cfcfb0e3938aab6c
Aayush Thakur <132382177+aayush0828@users.noreply.github.com>
2024-01-28 22:50:57 +0530

    Update car_api_service.dart

--- caridentifierapp/lib/car_api_service.dart
+++ caridentifierapp/lib/car_api_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:caridentifierapp/car_model.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http; 
 
 class CarApiService {
 static const String baseUrl = 'https://api.api-ninjas.com/v1/cars';
